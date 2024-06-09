3e4e74e48b73dc71bd058c40500991354bc429f3
michael-cox <2.cox.michael@gmail.com>
2024-06-08 23:55:28 -0500

    implement create_app

--- traction/app.py
+++ traction/app.py
@@ -1,3 +1,10 @@
 from flask import Flask
+import os
 
-app = Flask(__name__)
+def create_app(config=None, instance_path=None):
+    app = Flask(__name__, instance_path=instance_path, instance_relative_config=True)
+
+    if not os.path.exists(app.instance_path):
+        os.makedirs(app.instance_path)
+    
+    return app
